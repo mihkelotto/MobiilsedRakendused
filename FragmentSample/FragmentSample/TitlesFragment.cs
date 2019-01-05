6032d8257c4e3b1ca25e5a0885885a926c252eb2
mihkelotto <32923378+mihkelotto@users.noreply.github.com>
2019-01-05 11:03:41 +0200

    added fragments example
    added fragments example

---
+++ FragmentSample/FragmentSample/TitlesFragment.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace FragmentSample
+{
+    public class TitlesFragment : ListFragment
+    {
+        int selectedPlayId;
+
+        public TitlesFragment()
+        {
+
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+            ListAdapter = new ArrayAdapter<String>(Activity, Android.Resource.Layout.SimpleListItemActivated1, Shakespeare.Titles);
+
+            if (savedInstanceState != null)
+            {
+                selectedPlayId = savedInstanceState.GetInt("current_play_id", 0);
+            }
+        }
+
+        public override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            outState.PutInt("current_play_id", selectedPlayId);
+        }
+
+        public override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            ShowPlayQuote(position);
+        }
+
+        void ShowPlayQuote(int playId)
+        {
+            var intent = new Intent(Activity, typeof(PlayQuoteActivity));
+            intent.PutExtra("current_play_id", playId);
+            StartActivity(intent);
+        }
+
+        /*protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+        }*/
+        }
+}
